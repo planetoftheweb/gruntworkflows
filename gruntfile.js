09be37fbce5f73b2b1d40936283f9616c061851b
Ray Villalobos <ray@lynda.com>
2015-05-30 12:44:03 -0400

    01_07 Automating our workflow

--- gruntfile.js
+++ gruntfile.js
@@ -10,11 +10,39 @@ module.exports = function(grunt) {
         src: ['components/scripts/*.js'],
         dest: 'builds/development/js/script.js'
       }
+    }, //concat
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files : [{
+          src: 'components/sass/style.scss',
+          dest: 'builds/development/css/style.css'
+        }]
+      }
+    }, //sass
+
+    watch: {
+      options: {
+        spawn: false
+      },
+      scripts: {
+        files: ['builds/development/**/*.html',
+        'components/scripts/**/*.js',
+        'components/sass/**/*.scss'],
+        tasks: ['concat', 'sass']
+      }
     }
+
+
   }); //initConfig
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
+  grunt.registerTask('default', ['concat', 'sass', 'watch']);
 
 }; //wrapper function
